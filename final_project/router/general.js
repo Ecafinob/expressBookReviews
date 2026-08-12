9ca9fff5f99c3827411721cc07f33404b901a1a5
Ecafinob <ecafinobngameni@gmail.com>
2026-08-12 12:11:12 +0100

    Amélioration de la portée des tâches en utilisant async-await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require("axios");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -94,6 +95,59 @@ public_users.get('/review/:isbn',function (req, res) {
     });
   }
   return res.status(200).json(book.reviews);
+});
+
+//Routes avec axios
+
+//Tâche 10 liste des livres
+public_users.get("/", async(req,res) =>{
+  try{
+    const response = await axios.get("http://localhost:5000/");
+    res.status(200).json(response.data);
+  }catch(error){
+    res.status(500).json({
+      message:"Erreur lors de la récupération des livres"
+    });
+  }
+});
+
+//Tâche 11 obtenir un livre par son ISBN
+public_users.get("/isbn/:isbn", async (req,res) =>{
+  try{
+    const isbn = req.params.isbn;
+    const reqponse = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    res.status(200).json(response.data);
+  }catch(error){
+    res.status.apply(404).json({
+      message:"Livre non trouvé"
+    });
+  }
+});
+
+//Tâche 12 : obtenir les livres par auteur
+public_users.get("/author/:author", async (req,res) =>{
+  try{
+    const author = req.params.author;
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+  res.status(200).json(response.data);
+  }catch(error){
+    res.status(404).json({
+      message: "Livre non trouvé"
+    });
+  }
+});
+
+//Tâche 13: obtenir les livres par titre
+public_users.get("/title/:title", async (req, res) =>{
+  try{
+    const title = req.params.title;
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    res.status(200).json(response.data);
+  }catch(error){
+    res.status(404).json({
+      message: "Livre non trouvé"
+    });
+  }
 });
 
 module.exports.general = public_users;
